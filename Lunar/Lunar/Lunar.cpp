57389f4054ccdb28e479be800bd71c795348324b
as2120 <126.org@gmail.com>
2015-02-27 09:51:53 +0800

    150227

--- Lunar/Lunar/Lunar.cpp
+++ Lunar/Lunar/Lunar.cpp
@@ -1,7 +1,4 @@
-�?/ Lunar.cpp : 定义控制台应用程序的入口点�?
-//
-
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "Lunar.h"
 #include "C3C\ccc.h"
 
@@ -9,68 +6,60 @@
 #define new DEBUG_NEW
 #endif
 
-
-// 唯一的应用程序对�?
-
 CWinApp theApp;
 
 using namespace std;
 
 int CalcLunarDay()
 {
-    SYSTEMTIME st;
-    GetLocalTime(&st);
-    printf("请输入年月日 : \nSample : %04d:%02d:%02d\n\n",st.wYear,st.wMonth,st.wDay);
-    while(1)
-    {
-        int y,m,d = 0;
-        scanf("%d:%d:%d",&y,&m,&d);
-        if(d == 0)
-        {
-            break;
-        }
+	SYSTEMTIME st;
+	GetLocalTime(&st);
+	printf("请输入年月日 : \nSample : %04d:%02d:%02d\n\n",st.wYear,st.wMonth,st.wDay);
+	while(1)
+	{
+		int y,m,d = 0;
+		scanf("%d:%d:%d",&y,&m,&d);
+		if(d == 0)
+		{
+			break;
+		}
 
-        CCCC test(y,m,d);
-        /*printf("星期%s,%s,节日�?s,农历:%s�?s,农历节日�?s\n",test.dayinweek_Str,test.strchineseEra,test.strsFtvl,
-        	test.lunar->lunarMonth_Str,test.lunar->lunarDate_Str,
-        	test.lunar->strlunFtvl);*/
-        printf("[%04d.%02d.%02d] : 农历 %s�?s, 节日 %s, 农历节日 %s\n",
-               y,m,d,
-               test.lunar->lunarMonth_Str,test.lunar->lunarDate_Str,
-               test.strsFtvl,
-               test.lunar->strlunFtvl);
-    }
-    return 0;
+		CCCC test(y,m,d);
+
+		printf("[%04d.%02d.%02d] : 农历 %s%s, 节日 %s, 农历节日 %s\n",
+			y,m,d,
+			test.lunar->lunarMonth_Str,test.lunar->lunarDate_Str,
+			test.strsFtvl,
+			test.lunar->strlunFtvl);
+	}
+	return 0;
 }
 
 int CalcLunarForToday()
 {
-    SYSTEMTIME st;
-    GetLocalTime(&st);
+	SYSTEMTIME st;
+	GetLocalTime(&st);
 
-    CCCC LunarDay(st.wYear,st.wMonth,st.wDay);
-    printf("今日[%02d.%02d] : %s�?s\n",st.wMonth,st.wDay,LunarDay.lunar->lunarMonth_Str,LunarDay.lunar->lunarDate_Str);
+	CCCC LunarDay(st.wYear,st.wMonth,st.wDay);
+	printf("今日[%02d.%02d] : %s%s\n",st.wMonth,st.wDay,LunarDay.lunar->lunarMonth_Str,LunarDay.lunar->lunarDate_Str);
 
-    return 0;
+	return 0;
 }
 
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
-    int nRetCode = 0;
+	int nRetCode = 0;
 
-    // 初始�?MFC 并在失败时显示错�?
-    if (!AfxWinInit(::GetModuleHandle(NULL), NULL, ::GetCommandLine(), 0))
-    {
-        // TODO: 更改错误代码以符合您的需�?
-        _tprintf(_T("错误: MFC 初始化失败\n"));
-        nRetCode = 1;
-    }
-    else
-    {
-        // TODO: 在此处为应用程序的行为编写代码�?
-        CalcLunarForToday();
-        CalcLunarDay();
-    }
+	if (!AfxWinInit(::GetModuleHandle(NULL), NULL, ::GetCommandLine(), 0))
+	{
+		_tprintf(_T("错误: MFC 初始化失败\n"));
+		nRetCode = 1;
+	}
+	else
+	{
+		CalcLunarForToday();
+		CalcLunarDay();
+	}
 
-    return nRetCode;
+	return nRetCode;
 }
