21a53f0ae869c54c1796acd623c8f206471ef11d
as2120 <126.org@gmail.com>
2015-03-23 22:36:14 +0800

    boost.94

---
+++ Book/Boost C++ Application Development Cookbook/Chapter10/export_import/main.cpp
@@ -0,0 +1,14 @@
+﻿#include "../my_library/my_library.hpp"
+#include <cassert>
+
+int main()
+{
+    assert(foo() == 0);
+    bar b;
+    try
+    {
+        b.meow();
+        assert(false);
+    }
+    catch (const bar_exception&) {}
+}
