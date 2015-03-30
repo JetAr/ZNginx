5386e251368e69c7b45a38b09c20fddf91c51919
as2120 <126.org@gmail.com>
2015-03-30 15:23:33 +0800

    Format.153030

--- Lunar/Lunar/Lunar.cpp
+++ Lunar/Lunar/Lunar.cpp
@@ -12,54 +12,54 @@ using namespace std;
 
 int CalcLunarDay()
 {
-	SYSTEMTIME st;
-	GetLocalTime(&st);
-	printf("请输入年月日 : \nSample : %04d:%02d:%02d\n\n",st.wYear,st.wMonth,st.wDay);
-	while(1)
-	{
-		int y,m,d = 0;
-		scanf("%d:%d:%d",&y,&m,&d);
-		if(d == 0)
-		{
-			break;
-		}
+    SYSTEMTIME st;
+    GetLocalTime(&st);
+    printf("请输入年月日 : \nSample : %04d:%02d:%02d\n\n",st.wYear,st.wMonth,st.wDay);
+    while(1)
+    {
+        int y,m,d = 0;
+        scanf("%d:%d:%d",&y,&m,&d);
+        if(d == 0)
+        {
+            break;
+        }
 
-		CCCC test(y,m,d);
+        CCCC test(y,m,d);
 
-		printf("[%04d.%02d.%02d] : 农历 %s%s, 节日 %s, 农历节日 %s\n",
-			y,m,d,
-			test.lunar->lunarMonth_Str,test.lunar->lunarDate_Str,
-			test.strsFtvl,
-			test.lunar->strlunFtvl);
-	}
-	return 0;
+        printf("[%04d.%02d.%02d] : 农历 %s%s, 节日 %s, 农历节日 %s\n",
+               y,m,d,
+               test.lunar->lunarMonth_Str,test.lunar->lunarDate_Str,
+               test.strsFtvl,
+               test.lunar->strlunFtvl);
+    }
+    return 0;
 }
 
 int CalcLunarForToday()
 {
-	SYSTEMTIME st;
-	GetLocalTime(&st);
+    SYSTEMTIME st;
+    GetLocalTime(&st);
 
-	CCCC LunarDay(st.wYear,st.wMonth,st.wDay);
-	printf("今日[%02d.%02d] : %s%s\n",st.wMonth,st.wDay,LunarDay.lunar->lunarMonth_Str,LunarDay.lunar->lunarDate_Str);
+    CCCC LunarDay(st.wYear,st.wMonth,st.wDay);
+    printf("今日[%02d.%02d] : %s%s\n",st.wMonth,st.wDay,LunarDay.lunar->lunarMonth_Str,LunarDay.lunar->lunarDate_Str);
 
-	return 0;
+    return 0;
 }
 
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
-	int nRetCode = 0;
+    int nRetCode = 0;
 
-	if (!AfxWinInit(::GetModuleHandle(NULL), NULL, ::GetCommandLine(), 0))
-	{
-		_tprintf(_T("错误: MFC 初始化失败\n"));
-		nRetCode = 1;
-	}
-	else
-	{
-		CalcLunarForToday();
-		CalcLunarDay();
-	}
+    if (!AfxWinInit(::GetModuleHandle(NULL), NULL, ::GetCommandLine(), 0))
+    {
+        _tprintf(_T("错误: MFC 初始化失败\n"));
+        nRetCode = 1;
+    }
+    else
+    {
+        CalcLunarForToday();
+        CalcLunarDay();
+    }
 
-	return nRetCode;
+    return nRetCode;
 }
--- miniweb-win32-20130309/R208/libctb/src/kbhit.cpp
+++ miniweb-win32-20130309/R208/libctb/src/kbhit.cpp
@@ -5,36 +5,36 @@
 # include <termios.h>
 #endif
 
-namespace ctb
-{
+    namespace ctb
+    {
 
-char GetKey()
-{
+    char GetKey()
+    {
 #if defined ( WIN32 )
-    if(_kbhit())
-    {
-        return _getch();
-    }
-    return '\0';
+        if(_kbhit())
+        {
+            return _getch();
+        }
+        return '\0';
 #else
-    int ch;
-    static struct termios t, save_t;
-    tcgetattr(0,&t);
-    save_t = t;
-    t.c_lflag &= ~(ICANON);
-    t.c_cc[VMIN] = 0;
-    t.c_cc[VTIME] = 0;
-    tcsetattr(0,TCSANOW,&t);
+        int ch;
+        static struct termios t, save_t;
+        tcgetattr(0,&t);
+        save_t = t;
+        t.c_lflag &= ~(ICANON);
+        t.c_cc[VMIN] = 0;
+        t.c_cc[VTIME] = 0;
+        tcsetattr(0,TCSANOW,&t);
 
-    ch = fgetc(stdin);
+        ch = fgetc(stdin);
 
-    tcsetattr(0,TCSANOW,&save_t);
-    if(ch != EOF)
-    {
-        return ch;
-    }
-    return '\0';
+        tcsetattr(0,TCSANOW,&save_t);
+        if(ch != EOF)
+        {
+            return ch;
+        }
+        return '\0';
 #endif
-}
+    }
 
-} // namespace ctb
+    } // namespace ctb
