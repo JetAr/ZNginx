37804aad338b0fe8ca8302222953f8aacbb612ae
as2120 <126.org@gmail.com>
2015-04-01 11:33:15 +0800

    Pattern.150401

---
+++ Pattern/Cpp/Adaptor/adapter.cs
@@ -0,0 +1,391 @@
+﻿/* #FH  //z 2015-04-01 11:12:08 L.274'46072 BG57IV3@XCL T830831336 .K.F3489244081----+----+----+
++  0.FileInfo: +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+        File : E:\MyDoc\GitHub\ZNginx\Pattern\Cpp\Adaptor\adapter.cs.F3489244081+----+----+----+
+       Motto : No one can make you feel inferior without your consent.+----+----+----+----+----+
+       Motto : The death of fear is in doing what you fear to do.+----+----+----+----+----+----+
+       Motto : It's hard to beat a person who never gives up.----+----+----+----+----+----+----+
+       Motto : Take what you do seriously. Not yourself.----+----+----+----+----+----+----+----+
+       Motto : Life begins where your comfort zone ends.----+----+----+----+----+----+----+----+
+       Motto : To know and not do, is not yet to know.-+----+----+----+----+----+----+----+----+
+       Motto : The more you do , the more you can do.--+----+----+----+----+----+----+----+----+
+       Motto : Do what you do best, better.--+----+----+----+----+----+----+----+----+----+----+
+       Motto : Get Your Hands Dirty.----+----+----+----+----+----+----+----+----+----+----+----+
+      Author : 126.org@gmail.com (R) Kaizen (->30,T5,M&M,W)-+----+----+----+----+----+----+----+
+
++  1.ID & Desc: ----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  2.Req & Repro: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  3.Folder & File: +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  4.Cause & Solution: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  5.Todo & Done: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  6.Commit: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  7.Issue: ---+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  8.Summary: -+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  9.Misc: ----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
+***********************************************************************************************/
+
+#region 04-01 11:12 R#.0    @Ver.Info
+       Zey_S : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+		R#
+        zcurr
+        done
+        znote
+        zfav
+        zsum
+        zaddr
+        zkz
+        zpay
+        zlog
+        zdo
+        ztime
+        zmark
+        zkaizen
+        ztop
+        zbottom
+       Zey_E : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+       Zcl_S : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+       Zcl_E : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+     Zndex_S : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+     Zndex_E : //z 2015-04-01 11:30:43 L.274'44957 T833131431 .K ~66  +----+----+----+----+----+
+     TimeCnt : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+     Reg.Cnt : //z 2015-04-01 11:23:25 L.274'45395 T832053257 .K ~3   +----+----+----+----+----+
+     #3   R+ : //z 2015-04-01 11:23:25 L.274'45395 T832053257 .K ~3     V+.40   L+.286  
+     #2   R+ : //z 2015-04-01 11:13:51 L.274'45969 T830867431 .K ~2     V+.10   L+.202  
+     #1   R+ : //z 2015-04-01 11:12:10 L.274'46070 T830831361 .K ~1     V+.3    L+.62   
+     File.Op : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+     Version : //z 2015-04-01 11:30:43 L.274'44957 T833131431 .K ~69    R+.0    L+.39   --+----+
+     #3   V+ : //z 2015-04-01 11:29:15 L.274'45045 T832268846 .K ~57    R+.3    L+.344  
+     #2   R+ : //z 2015-04-01 11:23:25 L.274'45395 T832053257 .K ~40    R+.3    L+.287  V+.40   
+     #2   V+ : //z 2015-04-01 11:20:52 L.274'45548 T831945542 .K ~38    R+.1    L+.187  
+     #1   V+ : //z 2015-04-01 11:14:30 L.274'45930 T830903301 .K ~19    R+.2    L+.258  
+     #0   R+ : //z 2015-04-01 11:13:51 L.274'45969 T830867431 .K ~10    R+.2    L+.203  V+.10   
+     #0   R+ : //z 2015-04-01 11:12:10 L.274'46070 T830831361 .K ~3     R+.1    L+.63   V+.3    
+     K   Ter : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+     K  Mers : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+     K Kersi : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+     KVersio : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+     K Versi : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+     K  Vers : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+     K   Ver : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+     K    Ve : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+     K     V : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+     KaizenZ : //z 2015-04-01 11:12:08 L.274'46072 T830831336 .K ~0   +----+----+----+----+----+
+#endregion //z 2015-04-01 11:12:08 L.274'46072 BG57IV3@XCL T830831336 .K.F3489244081-+----+----+
+
+#region 04-01 11:12 R#.1    @Example of `adapter' design pattern in C++ （pazdera）
+01. 
+/*
+ * Example of `adapter' design pattern
+ * Copyright (C) 2011 Radek Pazdera
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <string>
+
+typedef int Cable; // wire with electrons :-)
+
+/* Adaptee (source) interface */
+class EuropeanSocketInterface
+{
+public:
+    virtual int voltage() = 0;
+
+    virtual Cable live() = 0;
+    virtual Cable neutral() = 0;
+    virtual Cable earth() = 0;
+};
+
+/* Adaptee */
+class Socket : public EuropeanSocketInterface
+{
+public:
+    int voltage()
+    {
+        return 230;
+    }
+
+    Cable live()
+    {
+        return 1;
+    }
+    Cable neutral()
+    {
+        return -1;
+    }
+    Cable earth()
+    {
+        return 0;
+    }
+};
+
+/* Target interface */
+class USASocketInterface
+{
+public:
+    virtual int voltage() = 0;
+
+    virtual Cable live() = 0;
+    virtual Cable neutral() = 0;
+};
+
+/* The Adapter */
+class Adapter : public USASocketInterface
+{
+    EuropeanSocketInterface* socket;
+
+public:
+    void plugIn(EuropeanSocketInterface* outlet)
+    {
+        socket = outlet;
+    }
+
+    int voltage()
+    {
+        return 110;
+    }
+    Cable live()
+    {
+        return socket->live();
+    }
+    Cable neutral()
+    {
+        return socket->neutral();
+    }
+};
+
+/* Client */
+class ElectricKettle
+{
+    USASocketInterface* power;
+
+public:
+    void plugIn(USASocketInterface* supply)
+    {
+        power = supply;
+    }
+
+    void boil()
+    {
+        if (power->voltage() > 110)
+        {
+            std::cout << "Kettle is on fire!" << std::endl;
+            return;
+        }
+
+        if (power->live() == 1 && power->neutral() == -1)
+        {
+            std::cout << "Coffee time!" << std::endl;
+        }
+    }
+};
+
+
+int main()
+{
+    Socket* socket = new Socket;//z 欧标
+    Adapter* adapter = new Adapter;//z 美标
+    ElectricKettle* kettle = new ElectricKettle;//z 需要美标作为电源供应
+
+    /* Pluging in. */
+    adapter->plugIn(socket);
+    kettle->plugIn(adapter);
+
+    /* Having coffee */
+    kettle->boil();
+
+    return 0;
+}
+#endregion //z 2015-04-01 11:12:10 L.274'46070 BG57IV3@XCL T830831361 .K.F3489244081-+----+----+
+
+#region 04-01 11:13 R#.2    @Code Examples - Chapter 7: The Adapter Pattern
+01. from 《Design Patterns Explained C++》
+Shape.h
+#pragma once
+
+class Shape
+{
+public:
+    Shape(void);
+    void virtual display()= 0;
+public:
+    ~Shape(void);
+};
+Shape.cpp
+#include "Shape.h"
+
+Shape::Shape(void)
+{
+}
+
+Shape::~Shape(void)
+{
+}
+
+Circle.h
+#pragma once
+#include "Shape.h"
+#include "XX_Circle.h"
+
+class Circle : public Shape
+{
+public:
+    Circle(XX_Circle *xxCircle);
+    void display();
+public:
+    ~Circle(void);
+private:
+    XX_Circle *myXX_Circle;
+};
+Circle.cpp
+#include "Circle.h"
+
+Circle::Circle(XX_Circle *xxCircle)
+{
+    myXX_Circle = xxCircle;
+}
+
+Circle::~Circle(void)
+{
+    delete myXX_Circle;
+}
+
+void Circle::display()
+{
+    myXX_Circle->displayIt();
+}
+
+XX_Circle.h
+#pragma once
+
+class XX_Circle
+{
+public:
+    XX_Circle(void);
+    void displayIt();
+public:
+    ~XX_Circle(void);
+};
+XX_Circle.cpp
+#include "XX_Circle.h"
+
+XX_Circle::XX_Circle(void)
+{
+}
+
+XX_Circle::~XX_Circle(void)
+{
+}
+#endregion //z 2015-04-01 11:13:51 L.274'45969 BG57IV3@XCL T830867431 .K.F3489244081-+----+----+
+
+#region 04-01 11:23 R#.3    @Adapter Design Pattern in C++
+01. 与第一个例子很类似
+2015-04-01 11:29 zlog 产品代码中很少见到此类写法。
+An adapter pattern converts the interface of a class into another interface the clients expect. Adapter lets classes work together that couldn't otherwise because of incompatible interfaces.
+
+It comprises three components:
+
+Target: This is the interface with which the client interacts.
+Adaptee: This is the interface the client wants to interact with, but can’t interact without the help of the Adapter.
+Adapter: This is derived from Target and contains the object of Adaptee.
+
+Class description:
+<AbstractPlug>: Abstract Target class
+<Plug>: Concrete Target class
+<AbstractSwitchBoard>: Abstract Adaptee class
+<SwitchBoard>: Concrete Adaptee class
+<Adapter>: Adapter class, our saviour
+
+// Abstract Target
+class AbstractPlug
+{
+public:
+    void virtual RoundPin() {}
+    void virtual PinCount() {}
+};
+
+// Concrete Target
+class Plug : public AbstractPlug
+{
+public:
+    void RoundPin()
+    {
+        cout << " I am Round Pin" << endl;
+    }
+    void PinCount()
+    {
+        cout << " I have two pins" << endl;
+    }
+};
+
+// Abstract Adaptee
+class AbstractSwitchBoard
+{
+public:
+    void virtual FlatPin() {}
+    void virtual PinCount() {}
+};
+
+// Concrete Adaptee
+class SwitchBoard : public AbstractSwitchBoard
+{
+public:
+    void FlatPin()
+    {
+        cout << " Flat Pin" << endl;
+    }
+    void PinCount()
+    {
+        cout << " I have three pins" << endl;
+    }
+};
+
+// Adapter
+class Adapter : public AbstractPlug
+{
+public:
+    AbstractSwitchBoard *T;
+    Adapter(AbstractSwitchBoard *TT)
+    {
+        T = TT;
+    }
+    void RoundPin()
+    {
+        T->FlatPin();
+    }
+    void PinCount()
+    {
+        T->PinCount();
+    }
+};
+
+// Client code
+void _tmain(int argc, _TCHAR* argv[])
+{
+    SwitchBoard *mySwitchBoard = new SwitchBoard; // Adaptee
+    // Target = Adapter(Adaptee)
+    AbstractPlug *adapter = new Adapter(mySwitchBoard);
+    adapter->RoundPin();
+    adapter->PinCount();
+}
+#endregion //z 2015-04-01 11:23:25 L.274'45395 BG57IV3@XCL T832053257 .K.F3489244081-+----+----+
