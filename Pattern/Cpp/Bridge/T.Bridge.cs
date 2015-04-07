ba49d67f566b11ae8a542d6a10f4e0b3b050a52a
as2120 <126.org@gmail.com>
2015-04-07 17:34:25 +0800

    Pattern & Bridge 150407

---
+++ Pattern/Cpp/Bridge/T.Bridge.cs
@@ -0,0 +1,198 @@
+﻿/* #FH  //z 2015-04-07 16:52:01 L.268'25679 BG57IV3@XCL T3404755824.K.F774980417 ----+----+----+
++  0.FileInfo: +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+        File : E:\MyDoc\GitHub\ZNginx\Pattern\Cpp\Bridge\T.Bridge.cs.F774980417 +----+----+----+
+       Motto : No one can make you feel inferior without your consent.+----+----+----+----+----+
+       Motto : The death of fear is in doing what you fear to do.+----+----+----+----+----+----+
+       Motto : It's hard to beat a person who never gives up.----+----+----+----+----+----+----+
+       Motto : Take what you do seriously. Not yourself.----+----+----+----+----+----+----+----+
+       Motto : Life begins where your comfort zone ends.----+----+----+----+----+----+----+----+
+       Motto : To know and not do, is not yet to know.-+----+----+----+----+----+----+----+----+
+       Motto : The more you do , the more you can do.--+----+----+----+----+----+----+----+----+
+       Motto : Do what you do best, better.--+----+----+----+----+----+----+----+----+----+----+
+       Motto : Get Your Hands Dirty.----+----+----+----+----+----+----+----+----+----+----+----+
+      Author : 126.org@gmail.com (R) Kaizen (->30,T5,M&M,W)-+----+----+----+----+----+----+----+
+
++  1.ID & Desc: ----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  2.Req & Repro: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  3.Folder & File: +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  4.Cause & Solution: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  5.Todo & Done: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  6.Commit: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  7.Issue: ---+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  8.Summary: -+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  9.Misc: ----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
+***********************************************************************************************/
+
+#region 04-07 16:52 R#.0    @Ver.Info
+       Zey_S : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+		R#
+       Zey_E : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+       Zcl_S : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+       Zcl_E : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+     Zndex_S : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+     Zndex_E : //z 2015-04-07 17:21:36 L.268'23904 T397662901 .K ~56  +----+----+----+----+----+
+     TimeCnt : //z 2015-04-07 16:57:16 L.268'25364 T3404935547.K ~1   +----+----+----+----+----+
+     Reg.Cnt : //z 2015-04-07 17:03:11 L.268'25009 T395362862 .K ~2   +----+----+----+----+----+
+     #2   R+ : //z 2015-04-07 17:03:11 L.268'25009 T395362862 .K ~2     V+.32   L+.191  
+     #1   R+ : //z 2015-04-07 16:52:04 L.268'25676 T3404755827.K ~1     V+.3    L+.62   
+     File.Op : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+     Version : //z 2015-04-07 17:21:36 L.268'23904 T397662901 .K ~58    R+.2    L+.199  --+----+
+     #3   V+ : //z 2015-04-07 17:21:32 L.268'23908 T397662897 .K ~57    R+.2    L+.199  
+     #2   V+ : //z 2015-04-07 17:16:42 L.268'24198 T396656694 .K ~38    R+.2    L+.193  
+     #1   R+ : //z 2015-04-07 17:03:11 L.268'25009 T395362862 .K ~32    R+.2    L+.192  V+.32   
+     #1   V+ : //z 2015-04-07 16:58:14 L.268'25306 T3404971482.K ~19    R+.1    L+.184  
+     #0   R+ : //z 2015-04-07 16:52:04 L.268'25676 T3404755827.K ~3     R+.1    L+.63   V+.3    
+     K   Ter : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+     K  Mers : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+     K Kersi : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+     KVersio : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+     K Versi : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+     K  Vers : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+     K   Ver : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+     K    Ve : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+     K     V : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+     KaizenZ : //z 2015-04-07 16:52:01 L.268'25679 T3404755824.K ~0   +----+----+----+----+----+
+#endregion //z 2015-04-07 16:52:01 L.268'25679 BG57IV3@XCL T3404755824.K.F774980417 -+----+----+
+
+#region 04-07 16:52 R#.1    @bridge pattern
+01. bridge pattern
+/*
+ * Example of `bridge' design pattern
+ * Copyright (C) 2011 Radek Pazdera
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#include <iostream>
+#include <string>
+
+/* Implemented interface. */
+class AbstractInterface
+{
+public:
+    virtual void someFunctionality() = 0;
+};
+
+/* Interface for internal implementation that Bridge uses. */
+class ImplementationInterface
+{
+public:
+    virtual void anotherFunctionality() = 0;
+};
+
+/* The Bridge */
+class Bridge : public AbstractInterface
+{
+protected:
+    ImplementationInterface* implementation;
+
+public:
+    Bridge(ImplementationInterface* backend)
+    {
+        implementation = backend;
+    }
+};
+
+/* Different special cases of the interface. */
+class UseCase1 : public Bridge
+{
+public:
+    UseCase1(ImplementationInterface* backend)
+        : Bridge(backend)
+    {}
+
+    void someFunctionality()
+    {
+        std::cout << "UseCase1 on ";
+        implementation->anotherFunctionality();
+    }
+};
+
+class UseCase2 : public Bridge
+{
+public:
+    UseCase2(ImplementationInterface* backend)
+        : Bridge(backend)
+    {}
+
+    void someFunctionality()
+    {
+        std::cout << "UseCase2 on ";
+        implementation->anotherFunctionality();
+    }
+};
+
+/* Different background implementations. */
+class Windows : public ImplementationInterface
+{
+public:
+    void anotherFunctionality()
+    {
+        std::cout << "Windows :-!" << std::endl;
+    }
+};
+
+class Linux : public ImplementationInterface
+{
+public:
+    void anotherFunctionality()
+    {
+        std::cout << "Linux! :-)" << std::endl;
+    }
+};
+
+int main()
+{
+    AbstractInterface *useCase = 0;
+    ImplementationInterface *osWindows = new Windows;
+    ImplementationInterface *osLinux = new Linux;
+
+    /* First case */
+    useCase = new UseCase1(osWindows);
+    useCase->someFunctionality();
+
+    useCase = new UseCase1(osLinux);
+    useCase->someFunctionality();
+
+    /* Second case */
+    useCase = new UseCase2(osWindows);
+    useCase->someFunctionality();
+
+    useCase = new UseCase2(osLinux);
+    useCase->someFunctionality();
+
+    return 0;
+}
+
+//z 2015-04-07 16:57:16 L.268'25364 T3404935547.K[T1,L185,R1,V12]
+2. 容易理解。
+re: 很重要的一个部分是弄明白其对应的适用场景的。
+#endregion //z 2015-04-07 16:52:04 L.268'25676 BG57IV3@XCL T3404755827.K.F774980417 -+----+----+
+
+#region 04-07 17:03 R#.2    @Bridging the gap between Interface and Implementation
+01. cp 上的例子
+http://www.codeproject.com/Articles/890/Bridge-Pattern-Bridging-the-gap-between-Interface
+
+Bridge and Adapter
+The structure of the Adapter Pattern (object adapter) may look similar to the Bridge Pattern. However, the adapter is meant to change the interface of an existing object and is mainly intended to make unrelated classes work together.
+#endregion //z 2015-04-07 17:03:11 L.268'25009 BG57IV3@XCL T395362862 .K.F774980417 -+----+----+
