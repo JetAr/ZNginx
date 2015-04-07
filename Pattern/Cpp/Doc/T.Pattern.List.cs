e0f39a809060b30d9aace1663a1a81b67a823c38
as2120 <126.org@gmail.com>
2015-04-07 18:02:14 +0800

    Doc

---
+++ Pattern/Cpp/Doc/T.Pattern.List.cs
@@ -0,0 +1,138 @@
+﻿/* #FH  //z 2015-04-07 17:51:49 L.268'22091 BG57IV3@XCL T401220700 .K.F2303716677----+----+----+
++  0.FileInfo: +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+        File : E:\MyDoc\GitHub\ZNginx\Pattern\Cpp\Doc\T.Pattern.List.cs.F2303716677--+----+----+
+       Motto : No one can make you feel inferior without your consent.+----+----+----+----+----+
+       Motto : The death of fear is in doing what you fear to do.+----+----+----+----+----+----+
+       Motto : It's hard to beat a person who never gives up.----+----+----+----+----+----+----+
+       Motto : Take what you do seriously. Not yourself.----+----+----+----+----+----+----+----+
+       Motto : Life begins where your comfort zone ends.----+----+----+----+----+----+----+----+
+       Motto : To know and not do, is not yet to know.-+----+----+----+----+----+----+----+----+
+       Motto : The more you do , the more you can do.--+----+----+----+----+----+----+----+----+
+       Motto : Do what you do best, better.--+----+----+----+----+----+----+----+----+----+----+
+       Motto : Get Your Hands Dirty.----+----+----+----+----+----+----+----+----+----+----+----+
+      Author : 126.org@gmail.com (R) Kaizen (->30,T5,M&M,W)-+----+----+----+----+----+----+----+
+
++  1.ID & Desc: ----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  2.Req & Repro: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  3.Folder & File: +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  4.Cause & Solution: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  5.Todo & Done: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  6.Commit: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  7.Issue: ---+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  8.Summary: -+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  9.Misc: ----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
+***********************************************************************************************/
+
+#region 04-07 17:51 R#.0    @Ver.Info
+       Zey_S : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+		R#
+       Zey_E : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+       Zcl_S : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+       Zcl_E : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+     Zndex_S : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+     Zndex_E : //z 2015-04-07 18:01:17 L.268'21523 T1686758963.K ~35  +----+----+----+----+----+
+     TimeCnt : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+     Reg.Cnt : //z 2015-04-07 17:55:43 L.268'21857 T401364442 .K ~2   +----+----+----+----+----+
+     #2   R+ : //z 2015-04-07 17:55:43 L.268'21857 T401364442 .K ~2     V+.32   L+.137  
+     #1   R+ : //z 2015-04-07 17:51:51 L.268'22089 T401220725 .K ~1     V+.3    L+.62   
+     File.Op : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+     Version : //z 2015-04-07 18:01:17 L.268'21523 T1686758963.K ~37    R+.2    L+.137  --+----+
+     #1   R+ : //z 2015-04-07 17:55:43 L.268'21857 T401364442 .K ~32    R+.2    L+.138  V+.32   
+     #1   V+ : //z 2015-04-07 17:52:28 L.268'22052 T401256570 .K ~19    R+.1    L+.100  
+     #0   R+ : //z 2015-04-07 17:51:51 L.268'22089 T401220725 .K ~3     R+.1    L+.63   V+.3    
+     K   Ter : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+     K  Mers : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+     K Kersi : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+     KVersio : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+     K Versi : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+     K  Vers : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+     K   Ver : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+     K    Ve : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+     K     V : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+     KaizenZ : //z 2015-04-07 17:51:49 L.268'22091 T401220700 .K ~0   +----+----+----+----+----+
+#endregion //z 2015-04-07 17:51:49 L.268'22091 BG57IV3@XCL T401220700 .K.F2303716677-+----+----+
+
+#region 04-07 17:51 R#.1    @找了个列表
+01. 
+1. Strategy:
+Defines a family of algorithms, encapsulates each one, and make them interchangeable. Strategy lets the algorithm vary independently from clients who use it.
+
+2. Decorator:
+Attach additional responsibilities to an object dynamically. Decorators provide a flexible alternative to subclassing for extending functionality.
+
+3. Factory Method
+Define an interface for creating an object, but let the subclasses decide which class to instantiate. Factory Method lets a class defer instantiation to subclasses.
+
+4. Observer
+Define a one-to-many dependency between objects so that when one object changes state, all its dependents are notified and updated automatically.
+
+5. Chain of Responsibility
+Avoid coupling the sender of a request to its receiver by giving more then one object a chance to handle the request. Chain the receiving objects and pass the request along the chain until an object handles it.
+
+6. Singleton
+Ensure a class only has one instance, and provide a global point of access to it.
+
+7. Flyweight
+Use sharing to support large numbers of fine-grained objects efficiently. A flyweight is a shared object that can be used in multiple contexts simultaneously. The flyweight acts as an independent object in each context; it’s indistinguishable from an instance of the object that’s not shared.
+
+8. Adapter
+Convert the interface of a class into another interface clients expect. Adapter lets classes work together that couldn’t otherwise because of incompatibility interfaces.
+
+9. Facade
+Provide a unified interface to a set of interfaces in a system. Fa?ade defines a higher-level interface that makes the subsystem easier to use.
+
+10. Template
+Define a skeleton of an algorithm in an operation, deferring some steps to subclasses. Template Method lets subclasses redefine certain steps of an algorithm without changing the algorithms structure.
+
+11. Builder
+Separate the construction of a complex object from its representation so that the same construction processes can create different representations.
+
+12. Iterator
+Provide a way to access the elements of an aggregate object sequentially without exposing its underlying representation.
+
+13. Composite
+Compose objects into tree structures to represent part-whole hierarchies. Composite lets clients treat individual objects and compositions of objects uniformly.
+
+14. Command
+Encapsulate a request as an object, thereby letting you parameterize clients with different requests, queue or log requests, and support undoable operations.
+
+15. Mediator
+Define an object that encapsulates how a set of objects interact. Mediator promotes loose coupling by keeping objects from referring to each other explicitly, and lets you vary their interaction independently.
+
+16. State
+Allow an object to alter its behavior when its internal state changes. The object will appear to change its class.
+
+17. Proxy
+Provide a surrogate or placeholder for another object to control access to it.
+
+18. Abstract Factory
+Provide an interface for creating families of related or dependent objects without specifying their concrete classes.
+
+19. Bridge
+Decouple an abstraction from its implementation so that the two can vary independently.
+
+20. Interpreter
+Given a language, define a representation for its grammar along with an interpreter that uses the representation to interpret sentences in the language.
+
+21. Memento
+Without violating encapsulation, capture and externalize an object’s internal state so that the object can be restored to this state later.
+
+22. Prototype
+Specify the kinds of objects to create using a prototypical instance, and create new objects by copying this prototype.
+
+23. Visitor
+Represent an operation to be performed on the elements of an object structure. Visitor lets you define a new operation without changing the classes of the elements on which it operates.
+#endregion //z 2015-04-07 17:51:51 L.268'22089 BG57IV3@XCL T401220725 .K.F2303716677-+----+----+
+
+#region 04-07 17:55 R#.2    @The 23 Gang of Four Design Patterns .. Revisited
+01. 
+#endregion //z 2015-04-07 17:55:43 L.268'21857 BG57IV3@XCL T401364442 .K.F2303716677-+----+----+
