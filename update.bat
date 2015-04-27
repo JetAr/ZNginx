4d5b64f9c9983b9c54781cd80a6ec74af4a62195
as2120 <126.org@gmail.com>
2015-04-27 11:46:21 +0800

    BUGFREE.Test.2015/04/27.11_46_21.03

---
+++ update.bat
@@ -0,0 +1,18 @@
+IF "%1" == "" ( echo "usage : update Comment" & goto END )
+
+@echo off
+set TDate=%DATE:~0,10%
+set THour=%TIME:~0,2%
+if %THour% LSS 10 set THour=0%TIME:~1,1%
+set TMin=%time:~3,2%
+set TSed=%time:~6,2%
+set TMs=%TIME:~-2%
+
+set FileTime=%1.%TDate%.%THour%_%TMin%_%TSed%.%TMs%
+
+git add .
+git commit -am "%FileTime%"
+git push
+
+:END
+@echo on
