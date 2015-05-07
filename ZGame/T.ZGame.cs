0bd409536ad673c7d386dc3e1d5586855ac45bed
as2120 <126.org@gmail.com>
2015-05-07 13:37:17 +0800

    Game_D.2015/05/07.13_37_16.73.K.MzM.T@WS

--- ZGame/T.ZGame.cs
+++ ZGame/T.ZGame.cs
@@ -39,12 +39,16 @@
        Zcl_S : //z 2015-05-07 13:13:39 L.238'38781 T857272694 .K ~0   +----+----+----+----+----+
        Zcl_E : //z 2015-05-07 13:13:39 L.238'38781 T857272694 .K ~0   +----+----+----+----+----+
      Zndex_S : //z 2015-05-07 13:13:39 L.238'38781 T857272694 .K ~0   +----+----+----+----+----+
-     Zndex_E : //z 2015-05-07 13:17:27 L.238'38553 T857416407 .K ~22  +----+----+----+----+----+
+      L63, 1:R#.1    @成长路径
+     Zndex_E : //z 2015-05-07 13:32:13 L.238'37667 T859608527 .K ~27  +----+----+----+----+----+
      TimeCnt : //z 2015-05-07 13:13:59 L.238'38761 T857272760 .K ~1   +----+----+----+----+----+
-     Reg.Cnt : //z 2015-05-07 13:13:44 L.238'38776 T857272722 .K ~1   +----+----+----+----+----+
+     Reg.Cnt : //z 2015-05-07 13:31:39 L.238'37701 T859572662 .K ~2   +----+----+----+----+----+
+     #2   R+ : //z 2015-05-07 13:31:39 L.238'37701 T859572662 .K ~2     V+.25   L+.82   
      #1   R+ : //z 2015-05-07 13:13:44 L.238'38776 T857272722 .K ~1     V+.3    L+.62   
-     File.Op : //z 2015-05-07 13:13:39 L.238'38781 T857272694 .K ~0   +----+----+----+----+----+
-     Version : //z 2015-05-07 13:17:27 L.238'38553 T857416407 .K ~23    R+.1    L+.74   --+----+
+     File.Op : //z 2015-05-07 13:31:33 L.238'37707 T859572656 .K ~1   +----+----+----+----+----+
+     #1   O+ : //z 2015-05-07 13:31:33 L.238'37707 T859572656 .K ~1     V+.24   L+.1    
+     Version : //z 2015-05-07 13:32:13 L.238'37667 T859608527 .K ~29    R+.2    L+.81   --+----+
+     #1   R+ : //z 2015-05-07 13:31:39 L.238'37701 T859572662 .K ~25    R+.2    L+.83   V+.25   
      #1   V+ : //z 2015-05-07 13:17:01 L.238'38579 T857416335 .K ~19    R+.1    L+.72   
      #0   R+ : //z 2015-05-07 13:13:44 L.238'38776 T857272722 .K ~3     R+.1    L+.63   V+.3    
      K   Ter : //z 2015-05-07 13:13:39 L.238'38781 T857272694 .K ~0   +----+----+----+----+----+
@@ -73,3 +77,43 @@ Real Time 3D Terrain Engines Using C++ And DX9
 Character Animation With Direct3D
 4. 具体引擎架构 Pro OGRE 3D Programming
 #endregion //z 2015-05-07 13:13:44 L.238'38776 BG57IV3@XCL T857272722 .K.F2482361782-+----+----+
+
+#region 05-07 13:31 R#.2    @一些游戏资料
+01. 
+渲染：DX11（luna)，3d图形编程基础-基于DX11，advanced graphics methods in opengl，opengl super bible， opengl4.0（packet)
+引擎：OSG，OGRE，Yake，illichilight，maratis，ngene，delta3d，ryzom，KlayGE，GameBryo，torque，unreal3，cryengine3，
+界面：DirectUI，CEGUI
+编辑器：WPF，MFC，托管C++
+脚本：Swig，PYthon，lua，C#（mono）
+物理：Box2D，bullet，havok，phyx
+AI：补充一个A-LIfe
+数值：matlab或mathimatica，c数值算法，fft算法
+游戏：魔域，墨香，天龙，苍天，luna，网狐，绝对女神，卓越之剑，魔兽开源私服
+并行：SSE，OpenMP，AMP，无锁编程
+汇编：Intel官网3卷，VC+MASM+嵌入式汇编
+模型: fbx sdk2013，DX11动态镶嵌
+声音：FMOD，OpenAL，XACT
+材质：Allgoarithmetic的substance air，designer
+地形：interlocking，GPU镶嵌，vtp，cryengine地形论文
+GPU计算：OpenCL，DirectXCompute，CUDA，相应书一搜就有，windows：windows核心编程第五版，windows internals
+debug：软件调试，advanced windows debugging，windbg，intel Vtune，PIX
+Web3D：webGL，alchemy，unity3d，activex，flash
+数学：XNA数学库
+boost：有三本书
+美：代码之美，xx之美，xx之美。。。。
+算法：树和图相关
+untiy3d：assetstore，官方forum
+场景管理：场景图，叉树，PVS等
+中间件：beast，speedtree，facefx，substance，umbra，xaitment，
+优化：代码优化，有效使用内存
+3D 绘图程序设计：使用Direct3D 10/9和OpenGL 2.0（CD）
+实时碰撞检测算法技术
+精通DirectX.3D图形与动画程序设计].王德才.扫描版.pdf - VeryCD... 
+GI:pbrt2,光线跟踪算法技术，radiosity，advanced global illumination
+OSG：王锐
+
+上面列出的是目前我所知道的一些对游戏编程有帮助的资料
+
+博客：ccancn，逍遥剑客，bugrunner,milo yip(0 bug事件).........
+网站：openGPU，nvidia，ati，intel，itpub(找书的好地方）,淘宝，搜客网，wiki（在上面搜开源商业引擎基本没有漏掉的）,verycd
+#endregion //z 2015-05-07 13:31:39 L.238'37701 BG57IV3@XCL T859572662 .K.F2482361782-+----+----+
