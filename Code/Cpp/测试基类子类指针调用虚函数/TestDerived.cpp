d2c064058e89c8e8b0afb516baf9040a635401bc
as2120 <126.org@gmail.com>
2015-05-21 15:53:56 +0800

    cpp_derived.2015/05/21.15_53_55.23.K.MzM.T@WS

---
+++ Code/Cpp/测试基类子类指针调用虚函数/TestDerived.cpp
@@ -0,0 +1,32 @@
+﻿//z 2015-05-21 15:53:00 L.224'29220 T3218067532.K F429593811 
+class Base
+{
+public:
+	virtual void A() {printf("Base:A()\r\n");}
+};
+
+class D1 : public Base
+{
+public:
+	void A() {printf("D1:A()\r\n");}
+};
+
+class D2 : public Base
+{
+public:
+	void A() {printf("D2:A()\r\n");}
+};
+
+void Callee(Base* pBase)
+{
+	pBase->A();
+}
+
+void Test_Caller()
+{
+	D1 * pD1 = new D1();
+	D2 * pD2 = new D2();
+
+	Callee(pD1);
+	Callee(pD2);
+}
