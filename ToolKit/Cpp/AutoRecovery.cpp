ca7c0a41951a1a1da4cdb6749c00131737196e2e
as2120 <126.org@gmail.com>
2015-08-20 11:05:36 +0800

    AutoRecovery.2015/08/20.11_05_34.86.K.MzM.T@WS

---
+++ ToolKit/Cpp/AutoRecovery.cpp
@@ -0,0 +1,28 @@
+﻿/*
+    2015-08-20 11:01 BG57IV3
+*/
+template <class T> 
+class CAutoRecovery
+{
+private:
+	T* m_pTarget;
+	T m_oFinalVal;
+
+private:
+	CAutoRecovery(CAutoRecovery const&);
+	CAutoRecovery& operator=(CAutoRecovery const&);
+
+public:
+	explicit CAutoRecovery(T* pTarget,const T& rNowVal,const T& rFinalVal)
+	{
+		m_pTarget = pTarget;
+		*m_pTarget = rNowVal;
+		m_oFinalVal = rFinalVal;
+	}
+
+	~CAutoRecovery()
+	{
+		*m_pTarget = m_oFinalVal;
+		m_pTarget = NULL;
+	}
+};
