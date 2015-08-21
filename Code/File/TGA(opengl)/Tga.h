e4904bb34fc18a86e2064291973c29cc122ee74f
as2120 <126.org@gmail.com>
2015-08-21 17:13:04 +0800

    tga.2015/08/21.17_12_49.04.K.MzM.T@WS

---
+++ Code/File/TGA(opengl)/Tga.h
@@ -0,0 +1,45 @@
+﻿#ifndef __TGA_H__
+#define __TGA_H__
+//
+//#pragma comment(lib, "Opengl32.lib")					//Link to OpenGL32.lib so we can use OpenGL stuff
+
+#include <windows.h>									// Standard windows header
+#include <stdio.h>										// Standard I/O header 
+#include <gl\gl.h>										// Header for OpenGL32 library
+
+
+typedef	struct									
+{
+	GLubyte	* imageData;									// Image Data (Up To 32 Bits)
+	GLuint	bpp;											// Image Color Depth In Bits Per Pixel
+	GLuint	width;											// Image Width
+	GLuint	height;											// Image Height
+	GLuint	texID;											// Texture ID Used To Select A Texture
+	GLuint	type;											// Image Type (GL_RGB, GL_RGBA)
+} Texture;	
+
+
+typedef struct
+{
+	GLubyte Header[12];									// TGA File Header
+} TGAHeader;
+
+
+typedef struct
+{
+	GLubyte		header[6];								// First 6 Useful Bytes From The Header
+	GLuint		bytesPerPixel;							// Holds Number Of Bytes Per Pixel Used In The TGA File
+	GLuint		imageSize;								// Used To Store The Image Size When Setting Aside Ram
+	GLuint		temp;									// Temporary Variable
+	GLuint		type;	
+	GLuint		Height;									//Height of Image
+	GLuint		Width;									//Width ofImage
+	GLuint		Bpp;									// Bits Per Pixel
+} TGA;
+
+
+bool LoadUncompressedTGA(Texture *, const TCHAR *, FILE *);	// Load an Uncompressed file
+bool LoadCompressedTGA(Texture *, const TCHAR *, FILE *);		// Load a Compressed file
+bool LoadTGA(Texture * texture, const TCHAR * filename);
+
+#endif
